c569678412819b7a7382cd36a777c0664da86d3b
EnzoFelyx <enzofelyx@gmail.com>
2024-06-16 20:19:22 -0300

    aula 2 criando site

---
+++ React/Aula_01/my-app/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logoIMG from '../../assets/logos.png';
+import './styles.css'
+
+class NavBar extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src={logoIMG} alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+          <ul className="nav-list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Thrending</a></li>
+            <li><a href="/">Categorias</a></li>
+            <li><a href="/">About us</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
--- React/Aula_01/my-app/src/index.js
+++ React/Aula_01/my-app/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> {/* fazer debug */}
     <App />
   </React.StrictMode>
 );
