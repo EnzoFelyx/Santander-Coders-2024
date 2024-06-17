8763dab32bca1412a0e1c9ce27a77a88e6da2cdc
EnzoFelyx <enzofelyx@gmail.com>
2024-06-16 21:20:32 -0300

    começando novo projeto

--- React/Aula_01/my-app/src/App.jsx
+++ React/Aula_01/my-app/src/App.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
       <>
         <NavBar />
         
-        <Counter/>
+        {/* <Counter/> */}
 
         <section id="articles">
 
---
+++ React/Aula_02/ciclo-de-vida/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Ciclo de Vida no React</h1>
+    </div>
+  );
+}
+
+export default App;
